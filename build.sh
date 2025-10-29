05a9805e6282f077f56289c6c5bcdaa93cda3e1f
dvictoriano <darlynvictoriano@gmail.com>
2025-10-29 15:54:00 -0400

    feat: ajustes

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo "Building the project..."
+
+## Set up the environment
+if [ ! -d "env" ]; then
+    python3 -m venv env
+    pip install -r requirements.txt
+fi
+
+## Activate the virtual environment
+source env/bin/activate
+
+# Build the project
+cd web
+mkdocs build
+cd ..
+
+## Deactivate the virtual environment
+deactivate
+echo "Build complete."
+exit 0
